c25fc6598123cc6eefc53ac79826bff9e331d23d
dhseo <birdblues@mac.com>
2023-10-26 15:43:23 +0900

    test1

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+# app.py
+import dash
+from dash import dcc
+from dash import html
+from flask import Flask
 
-app = Flask(__name__)
+server = Flask(__name__)
+app = dash.Dash(
+    __name__,
+    server=server,
+)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+app.layout = html.Div([
+    dcc.Graph(id='example-graph', figure={
+        'data': [{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                 ],
+        'layout': {'title': 'Dash Data Visualization'}
+    })
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
