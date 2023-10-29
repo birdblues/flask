ae49ac71e5393861d15231d23f0984d9a91b7c44
Dong Hwan Seo <birdblues@mac.com>
2023-10-29 19:01:11 +0900

    test10

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import json
+import numpy as np
+import yfinance as yf
+import pandas as pd
+import pandas_ta as ta
 import random
+from pprint import pprint
 from datetime import datetime, timedelta
 
 import dash_tvlwc
@@ -9,22 +15,60 @@ from dash import html, dcc, ctx
 from dash_tvlwc.types import ColorType, SeriesType
 from data_generator import generate_random_ohlc, generate_random_series
 
+df = yf.Ticker('KRW=X').history(period='24mo', interval='1d')[['Open', 'High', 'Low', 'Close', 'Volume']]
+df = df.reset_index()
+df.columns = ['time','open','high','low','close','volume']
+df['time'] = df['time'].dt.strftime('%Y-%m-%d')  
+# pprint(df)
+data = [json.loads(
+    df.filter(['time','close'], axis=1)
+      .rename(columns={"close": "value"})
+      .to_json(orient = "records"))]
+# pprint(data)
+
 main_panel = [
     html.Div(style={'position': 'relative', 'width': '100%', 'height': '100%'}, children=[
         html.Div(children=[
             dash_tvlwc.Tvlwc(
                 id='tv-chart-1',
-                seriesData=[generate_random_ohlc(100, n=100)],
-                seriesTypes=[SeriesType.Candlestick],
-                width='99%',
+                seriesData=data,
+                seriesTypes=[SeriesType.Area],
+                width='100%',
                 chartOptions={
-                    'layout': {
-                        'background': {'type': ColorType.Solid, 'color': '#1B2631'},
-                        'textColor': 'white',
+                    "handleScale": {
+                        "axisPressedMouseMove": False,
+                        "mouseWheel": True,
+                        "pinch": False
                     },
-                    'grid': {
-                        'vertLines': {'visible': True, 'color': 'rgba(255,255,255,0.1)'},
-                        'horzLines': {'visible': True, 'color': 'rgba(255,255,255,0.1)'},
+                    "timeScale": {
+                        "borderColor": "rgba(197, 203, 206, 0.8)",
+                        "barSpacing": 10,
+                        "minBarSpacing": 2,
+                        "timeVisible": False,
+                        "secondsVisible": False,
+                    },
+                    "watermark": {
+                        "visible": True,
+                        "fontSize": 50,
+                        "horzAlign": 'center',
+                        "vertAlign": 'center',
+                        "color": 'rgba(255, 255, 255, 0.2)',
+                        "text": 'USD/KRW',
+                    },
+                    "layout": {
+                        "background": {
+                            "type": 'solid',
+                            "color": '#131722'
+                        },
+                        "textColor": '#d1d4dc',
+                    },
+                    "grid": {
+                        "vertLines": {
+                            "color": 'rgba(42, 46, 57, 0)',
+                        },
+                        "horzLines": {
+                            "color": 'rgba(42, 46, 57, 0.6)',
+                        }
                     },
                     'localization': {
                         'locale': 'en-US',
@@ -37,22 +81,10 @@ main_panel = [
         html.Div(id='chart-info', children=[
             html.Span(id='chart-price', style={'fontSize': '60px', 'fontWeight': 'bold'}),
             html.Span(id='chart-date', style={'fontSize': 'small'}),
-        ], style={'position': 'absolute', 'left': 0, 'top': 0, 'zIndex': 10, 'color': 'white', 'padding': '10px'})
+        ], style={'position': 'absolute', 'left': 0, 'top': 0, 'zIndex': 10, 'color': 'white', 'padding': '0px'})
     ]),
    
 ]
-
-chart_options = {
-    'layout': {
-        'background': {'type': 'solid', 'color': '#000000'},
-        'textColor': 'white',
-    },
-    'grid': {
-        'vertLines': {'visible': False},
-        'horzLines': {'visible': False},
-    },
-    'localization': {'locale': 'en-US'}
-}
 
 app = dash.Dash(__name__, external_stylesheets=['./assets/stylesheet.css'])
 app.layout = html.Div([
@@ -77,34 +109,6 @@ app.layout = html.Div([
     ],
     prevent_initial_call=True
 )
-def change_props(n, current_chart_options, chart_info_style):
-    if current_chart_options['layout']['background']['color'] == '#1B2631':
-        current_chart_options = {
-            'layout': {
-                'background': {'type': ColorType.Solid, 'color': '#dddddd'},
-                'textColor': '#111111',
-            },
-            'grid': {
-                'vertLines': {'visible': True, 'color': 'rgba(0,0,0,0.1)'},
-                'horzLines': {'visible': True, 'color': 'rgba(0,0,0,0.1)'},
-            }
-        }
-        chart_info_style['color'] = '#111111'
-    else:
-        current_chart_options = {
-            'layout': {
-                'background': {'type': ColorType.Solid, 'color': '#1B2631'},
-                'textColor': 'white',
-            },
-            'grid': {
-                'vertLines': {'visible': True, 'color': 'rgba(255,255,255,0.1)'},
-                'horzLines': {'visible': True, 'color': 'rgba(255,255,255,0.1)'},
-            },
-        }
-        chart_info_style['color'] = '#bbbbbb'
-
-    return [current_chart_options, chart_info_style]
-
 
 @app.callback(
     [
@@ -121,36 +125,6 @@ def change_props(n, current_chart_options, chart_info_style):
     ],
     prevent_initial_call=True
 )
-def change_props(n, interval, series_data, series_type):
-    if ctx.triggered_id == 'timer':
-        last_close_date = series_data[0][-1]['time']
-        last_close_dt = datetime(last_close_date['year'], last_close_date['month'], last_close_date['day'])
-        if series_type == [SeriesType.Candlestick]:
-            new_datapoint = generate_random_ohlc(
-                t0=(last_close_dt + timedelta(days=1)).strftime('%Y-%m-%d'),
-                v0=series_data[0][-1]['close'], n=1
-            )
-            series_data[0].extend(new_datapoint)
-        else:
-            new_datapoint = generate_random_series(
-                t0=(last_close_dt + timedelta(days=1)).strftime('%Y-%m-%d'),
-                v0=series_data[0][-1]['value'], n=1
-            )
-            series_data[0].extend(new_datapoint)
-        series_data[0] = series_data[0][1:]
-        return [series_data, series_type]
-
-    elif ctx.triggered_id == 'change-chart-type':
-        if series_type == [SeriesType.Candlestick]:
-            series_type = [SeriesType.Line]
-            series_data = [generate_random_series(100, n=100)]
-        else:
-            series_type = [SeriesType.Candlestick]
-            series_data = [generate_random_ohlc(100, n=100)]
-        return [series_data, series_type]
-
-
-
 @app.callback(
     [
         Output('chart-date', 'children'),
